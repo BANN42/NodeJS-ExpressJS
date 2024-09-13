103d4b419af7f20e7670b4089cb5489e28bde571
Ban  42 <151378495+BANN42@users.noreply.github.com>
2024-09-13 12:14:47 +0200

    Set the Enviremenet
    < GET one/all Books from Books.json(as BackendDB)

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-const http = require('node:http');
-const server  = http.createServer((req, res) => {
-     res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const express = require('express');
+const fs = require('node:fs');
+const app = express();
 const port = 3000;
 
-server.listen(port, () => {
-     console.log(`Server running at http://localhost:${port}/`);
-     });
-     
+app.use(express.json());
+
+const ReadFileSync = (filePath) => {
+     return fs.readFileSync(filePath, 'utf8');
+}
+
+const BooksList = JSON.parse(ReadFileSync('books.json'));
+
+
+
+
+
+// get All Books
+app.get('/api/books', function(req, res){
+     res.json(BooksList).status(200);
+     res.end();
+}) 
+
+// get a single book by id
+app.get('/api/books/:id', function(req, res){
+     const bookId = +req.params.id;
+     const book = BooksList.find(book => book.id === bookId);
+     if(book){
+          res.json(book).status(200);
+     }else{
+          res.status(404).json({message: 'Book not found'});
+     }
+     res.end();
+})
+
+
+
+app.listen(port, () => {
+          console.log(`Server running on port ${port}`);
+});
